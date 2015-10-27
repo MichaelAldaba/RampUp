0ccf8ddbcb34d66dcebecd55a46c8c3b655f77ac
Michael Aldaba <maldaba3@gmail.com>
2015-10-27 18:36:30 -0400

    Create resolution class for comparables

---
+++ resolution.rb
@@ -0,0 +1,13 @@
+class Resolution
+	def compare
+		"scissors" <=> "rock"
+		"rock" <=> "spock"
+		"spock" <=> "paper"
+		"paper" <=> "lizard"
+		"lizard" <=> "scissors"
+		"scissors" <=> "spock"
+		"spock" <=> "lizard"
+		"lizard" <=> "rock"
+		"rock" <=> "paper"
+		"paper" <=> "scissors"
+	end
