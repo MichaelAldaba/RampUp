312ab0447fc26e5fcd3e6882df235faa0eb352f5
Michael Aldaba <maldaba3@gmail.com>
2015-11-11 19:38:01 -0500

    Add name_age and temperature folders

---
+++ name_age/name_age.rb
@@ -0,0 +1,25 @@
+=begin
+ask for name
+ask for age
+ask has celebrated birthday
+puts name and year of birth
+=end
+
+def input_name_age
+	puts "\nWhat is your name?"
+	name = gets.chomp.capitalize
+
+	puts "\nWhat is your age?"
+	age = gets.chomp.to_i
+
+	puts "\nDid you celebrate your birthday this year? Yes or no?"
+	input = gets.chomp
+
+	if input == "yes"
+		puts "#{name} was born in #{Time.now.year - age}"
+	else
+		puts "#{name} was born in #{Time.now.year - 1 - age}"
+	end
+end
+
+input_name_age
